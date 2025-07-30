208be5c7c6162d771e2cdd132ce402085729e934
Stefan Boca <stefan.r.boca@gmail.com>
2025-07-30 15:06:51 -0700

    feat: hours estimation

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
-use std::{collections::HashMap, path::PathBuf};
+use std::{
+    collections::{HashMap, HashSet},
+    path::PathBuf,
+};
 
 use anyhow::bail;
 use clap::Parser;
+use gix::bstr::BString;
 
 /// Estimate hours of a project
 #[derive(Debug, Parser, Clone)]
 #[command(version, long_about = None)]
 struct Args {
     /// Maximum time difference between two subsequent commits in minutes which are counted to be
-    /// in the same coding "session"
+    /// in the same coding session
     #[arg(short = 'd', long, default_value_t = 2 * 60)]
     max_commit_diff: u32,
 
-    /// How many minutes should be added for the first commit of the coding session
+    /// How many minutes should be added for the first commit of each coding session
     #[arg(short, long, default_value_t = 2 * 60)]
     first_commit_add: u32,
 
@@ -39,10 +43,10 @@ struct Args {
     branch: Option<String>,
 }
 
-fn get_commits(
+fn get_commit_times_by_author(
     args: &Args,
     repo: &gix::Repository,
-) -> anyhow::Result<HashMap<gix::ObjectId, (String, String, gix::date::Time)>> {
+) -> anyhow::Result<HashMap<BString, Vec<gix::date::Time>>> {
     let refs = repo.references()?;
     let prefix = if let Some(branch) = &args.branch {
         format!("refs/heads/{branch}")
@@ -51,7 +55,8 @@ fn get_commits(
     };
     let heads = refs.prefixed(prefix.as_str())?;
 
-    let mut commits = HashMap::new();
+    let mut visited = HashSet::new();
+    let mut times_by_author: HashMap<BString, Vec<gix::date::Time>> = HashMap::new();
     for head in heads.filter_map(|h| h.ok()) {
         let mut stack = vec![head.id()];
         while let Some(id) = stack.pop() {
@@ -59,11 +64,12 @@ fn get_commits(
                 continue;
             };
 
-            if commits.contains_key(&commit.id) {
+            if visited.contains(&commit.id) {
                 // This commit and its parents have already been visited. Any further work is
                 // redundant.
                 continue;
             }
+            visited.insert(commit.id);
 
             let stack_len = stack.len();
             // extend the stack directly to avoid allocating a temporary vec for the parents.
@@ -75,17 +81,46 @@ fn get_commits(
             {
                 let is_merge = num_parents > 1;
                 if !is_merge || args.merge_commits {
-                    // TODO: filter by since/until
-                    commits.insert(
-                        commit.id,
-                        (author.name.to_string(), author.email.to_string(), time),
-                    );
+                    // TODO:
+                    // - filter by since/until
+                    // - consider using name instead of email (or both?)
+                    // - email(/name) aliases
+                    if let Some(times) = times_by_author.get_mut(author.email) {
+                        times.push(time);
+                    } else {
+                        times_by_author.insert(author.email.into(), vec![time]);
+                    }
                 }
             }
         }
     }
 
-    Ok(commits)
+    for times in times_by_author.values_mut() {
+        times.sort();
+    }
+
+    Ok(times_by_author)
+}
+
+fn estimate_hours(args: &Args, times: &[gix::date::Time]) -> u32 {
+    if times.len() < 2 {
+        return 0;
+    }
+
+    let mut hours = 10f64;
+
+    for window in times.windows(2) {
+        let (time, next_time) = (window[0], window[1]);
+        let diff_in_minutes = (next_time.seconds - time.seconds) as f64 / 60.0;
+
+        if diff_in_minutes < args.max_commit_diff as f64 {
+            hours += diff_in_minutes / 60.0;
+        } else {
+            hours += args.first_commit_add as f64 / 60.0;
+        }
+    }
+
+    hours.round() as u32
 }
 
 fn main() -> anyhow::Result<()> {
@@ -97,7 +132,13 @@ fn main() -> anyhow::Result<()> {
 
     let args = Args::parse();
     let repo = gix::open(&args.path)?;
-    let commits = get_commits(&args, &repo)?;
+    for (author, times) in get_commit_times_by_author(&args, &repo)? {
+        println!(
+            "{author}: {} commits, {} hours",
+            times.len(),
+            estimate_hours(&args, &times)
+        );
+    }
 
     Ok(())
 }
